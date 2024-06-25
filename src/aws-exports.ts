a8a8fb69a89ec33fee6cd69ca6c191e46c61b0d2
cm-saiki-kyohei <57649503+cm-saiki-kyohei@users.noreply.github>
2024-06-25 17:07:07 +0900

    認証を追加

---
+++ src/aws-exports.ts
@@ -0,0 +1,8 @@
+const awsConfig = {
+    aws_project_region: import.meta.env.VITE_AWS_PROJECT_REGION,
+    aws_cognito_region: import.meta.env.VITE_AWS_COGNITO_REGION,
+    aws_user_pools_id: import.meta.env.VITE_AWS_USER_POOLS_ID,
+    aws_user_pools_web_client_id: import.meta.env.VITE_AWS_USER_POOLS_WEB_CLIENT_ID,
+};
+
+export default awsConfig;
