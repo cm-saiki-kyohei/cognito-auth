dcdcff6fafccb9d68bdcb9b0a28aaf68079edd3a
cm-saiki-kyohei <57649503+cm-saiki-kyohei@users.noreply.github>
2024-06-26 14:56:51 +0900

    id tokenを表示するように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import { Amplify } from 'aws-amplify';
+import { useEffect, useState } from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
 import awsConfig from './aws-exports';
 
 Amplify.configure(awsConfig);
 
 function App() {
+  const [idToken, setIdToken] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchIdTokenFromLocalStorage = () => {
+      const keyPrefix = `CognitoIdentityServiceProvider.${awsConfig.aws_user_pools_web_client_id}`;
+      const lastAuthUser = localStorage.getItem(`${keyPrefix}.LastAuthUser`);
+      const idToken = lastAuthUser ? localStorage.getItem(`${keyPrefix}.${lastAuthUser}.idToken`) : null;
+
+      idToken ? setIdToken(idToken) : setError('Failed to retrieve ID token from localStorage');
+    };
+
+    fetchIdTokenFromLocalStorage();
+  }, []);
+
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user?.username}</h1>
+          {error && (
+            <div style={{ color: 'red' }}>
+              <h2>Error:</h2>
+              <pre>{error}</pre>
+            </div>
+          )}
+          {idToken && (
+            <div>
+              <h2>ID Token:</h2>
+              <pre>{idToken}</pre>
+            </div>
+          )}
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
